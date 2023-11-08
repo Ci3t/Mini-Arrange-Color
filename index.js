d661d6830d2fcc3b38804e2df9f8f4b410d3ceb1
Ci3t <ranigfx@gmail.com>
2023-11-09 01:27:42 +0200

    done

--- index.js
+++ index.js
@@ -14,11 +14,10 @@ const greenCounter = document.getElementById("green-counter");
 
 let currentColor = null;
 const inventory = {
-    red: 0,
-    blue: 0,
-    green: 0,
+  red: 0,
+  blue: 0,
+  green: 0,
 };
-
 
 // const arrayColors = ["red", "blue", "green"]
 // // Initialize the grid with random colors and update the inventory
@@ -33,110 +32,99 @@ const inventory = {
 
 // }
 
-const arrayColors = ["red", "blue", "green"];
+// const arrayColors = ["red", "blue", "green"];
 const numRows = 5; // Number of rows
 const numCols = 12; // Number of columns
 
 const gridSize = 100; // New size for each square in pixels
 
 function gridSizeStyle(numCols, numRows, gridSize) {
-    grid.style.display = "grid";
-    grid.style.gridTemplateColumns = `repeat(${numCols}, ${gridSize}px)`;
-    grid.style.gridTemplateRows = `repeat(${numRows}, ${gridSize}px)`;
+  grid.style.display = "grid";
+  grid.style.gridTemplateColumns = `repeat(${numCols}, ${gridSize}px)`;
+  grid.style.gridTemplateRows = `repeat(${numRows}, ${gridSize}px)`;
 }
-function createDynamicGrid(numRows, numCols, gridSize, arrayColors) {
-    const gridData = [];
+function createDynamicGrid(numRows, numCols, gridSize) {
+  const gridData = [];
 
-    gridSizeStyle(numCols, numRows, gridSize)
+  gridSizeStyle(numCols, numRows, gridSize);
 
-    for (let i = 0; i < numRows; i++) {
-        // const row = [];
-        for (let j = 0; j < numCols; j++) {
-            const gridItem = document.createElement("div");
-            gridItem.className = "grid-item";
-            gridItem.id = `${i}-${j}`
-            grid.appendChild(gridItem);
-            // const randomColor = arrayColors[Math.floor(Math.random() * arrayColors.length)];
-            // gridItem.style.backgroundColor = randomColor;
+  for (let i = 0; i < numRows; i++) {
+    // const row = [];
+    for (let j = 0; j < numCols; j++) {
+      const gridItem = document.createElement("div");
+      gridItem.className = "grid-item";
+      gridItem.id = `${i}-${j}`;
+      grid.appendChild(gridItem);
+      // const randomColor = arrayColors[Math.floor(Math.random() * arrayColors.length)];
+      // gridItem.style.backgroundColor = randomColor;
 
-            // row.push(randomColor);
-            gridData.push(gridItem);
-        }
+      // row.push(randomColor);
+      gridData.push(gridItem);
     }
-    return gridData;
+  }
+  return gridData;
 }
-const gridData = createDynamicGrid(numRows, numCols, gridSize, arrayColors);
+const gridData = createDynamicGrid(numRows, numCols, gridSize);
 
 function createGridColors() {
-
-    for (let g = 0; g < gridData.length; g++) {
-
-        let [i, j] = gridData[g].getAttribute("id").split("-")
-        drawRed(Number(i), Number(j), Number(g))
-        drawBlue(Number(i), Number(j), Number(g))
-        drawGreen(Number(i), Number(j), Number(g))
-
-    }
+  for (let g = 0; g < gridData.length; g++) {
+    let [i, j] = gridData[g].getAttribute("id").split("-");
+    drawRed(Number(i), Number(j), Number(g));
+    drawBlue(Number(i), Number(j), Number(g));
+    drawGreen(Number(i), Number(j), Number(g));
+  }
 }
 
-createGridColors()
+createGridColors();
 
 function drawRed(i, j, g) {
-
-    if (i === 0 && j >= 0) {
-        // gridData[g].classList.add("red")
-        gridData[g].style.backgroundColor = "red"
-    }
-    if (i === 4 && j <= 11) {
-        // gridData[g].classList.add("red")
-        gridData[g].style.backgroundColor = "red"
-    }
-    if (i >= 0 && j === 0) {
-        gridData[g].style.backgroundColor = "red"
-
-    }
-    if (i >= 0 && j === 11) {
-        gridData[g].style.backgroundColor = "red"
-
-    }
+  if (i === 0 && j >= 0) {
+    // gridData[g].classList.add("red")
+    gridData[g].style.backgroundColor = "red";
+  }
+  if (i === 4 && j <= 11) {
+    // gridData[g].classList.add("red")
+    gridData[g].style.backgroundColor = "red";
+  }
+  if (i >= 0 && j === 0) {
+    gridData[g].style.backgroundColor = "red";
+  }
+  if (i >= 0 && j === 11) {
+    gridData[g].style.backgroundColor = "red";
+  }
 }
 function drawBlue(i, j, g) {
+  if (i > 0 && i < 4 && j > 0 && j < 4) {
+    // gridData[g].classList.add("red")
+    gridData[g].style.backgroundColor = "blue";
+  }
+  if (i === 2 && j === 2) {
+    // gridData[g].classList.add("red")
+    gridData[g].style.backgroundColor = "green";
+  }
 
-    if (i > 0 && i < 4 && j > 0 && j < 4) {
-        // gridData[g].classList.add("red")
-        gridData[g].style.backgroundColor = "blue"
-    }
-    if (i === 2 && j === 2) {
-        // gridData[g].classList.add("red")
-        gridData[g].style.backgroundColor = "green"
-    }
-
-    if (i > 0 && i < 4 && j > 4 && j < 8) {
-        // gridData[g].classList.add("red")
-        gridData[g].style.backgroundColor = "blue"
-    }
-    if (i === 2 && j === 6) {
-        // gridData[g].classList.add("red")
-        gridData[g].style.backgroundColor = "green"
-    }
-
+  if (i > 0 && i < 4 && j > 4 && j < 8) {
+    // gridData[g].classList.add("red")
+    gridData[g].style.backgroundColor = "blue";
+  }
+  if (i === 2 && j === 6) {
+    // gridData[g].classList.add("red")
+    gridData[g].style.backgroundColor = "green";
+  }
 }
 function drawGreen(i, j, g) {
-
-
-    if (i > 0 && i < 4 && j > 7 && j < 11) {
-        // gridData[g].classList.add("red")
-        gridData[g].style.backgroundColor = "green"
-    }
-    if (i === 2 && j === 9) {
-        // gridData[g].classList.add("red")
-        gridData[g].style.backgroundColor = "yellow"
-    }
-    if (i > 0 && i < 4 && j === 4) {
-        // gridData[g].classList.add("red")
-        gridData[g].style.backgroundColor = "yellow"
-    }
-
+  if (i > 0 && i < 4 && j > 7 && j < 11) {
+    // gridData[g].classList.add("red")
+    gridData[g].style.backgroundColor = "green";
+  }
+  if (i === 2 && j === 9) {
+    // gridData[g].classList.add("red")
+    gridData[g].style.backgroundColor = "yellow";
+  }
+  if (i > 0 && i < 4 && j === 4) {
+    // gridData[g].classList.add("red")
+    gridData[g].style.backgroundColor = "yellow";
+  }
 }
 
 redSquare.style.backgroundColor = "red";
@@ -144,173 +132,93 @@ blueSquare.style.backgroundColor = "blue";
 greenSquare.style.backgroundColor = "green";
 
 function updateCounters() {
-    redCounter.textContent = inventory.red;
-    blueCounter.textContent = inventory.blue;
-    greenCounter.textContent = inventory.green;
+  redCounter.textContent = inventory.red;
+  blueCounter.textContent = inventory.blue;
+  greenCounter.textContent = inventory.green;
 }
 
 function highLightColor(color) {
-    color.style.border = "yellow"
-    color.style.borderStyle = "solid"
-    color.style.borderWidth = "2px"
+  color.style.border = "yellow";
+  color.style.borderStyle = "solid";
+  color.style.borderWidth = "2px";
 }
 function removeHighLightColor(color) {
-    color.style.border = "#ccc"
-    color.style.borderStyle = "solid"
-    color.style.borderWidth = "1px"
+  color.style.border = "#ccc";
+  color.style.borderStyle = "solid";
+  color.style.borderWidth = "1px";
 }
 
-
 tools.addEventListener("click", (e) => {
-    const target = e.target.id
+  const target = e.target.id;
 
-    target === "eraser" ? currentColor = "eraser" : currentColor = "whiteboard";
+  if (target === "eraser") {
+    currentColor = "eraser";
+    selectColor("eraser", eraser);
+  }
+  if (target === "whiteboard") {
+    currentColor = "whiteboard";
+    selectColor("whiteboard", whiteboard);
+  }
 
+  // e.stopPropagation(); // Prevent the click event from bubbling
 });
 
 // ! inv event listner
 
-// inv.addEventListener("click", (e) => {
-//     const target = e.target.id
-
-//     if (target === "red") {
-//         highLightColor(redSquare)
-//         removeHighLightColor(blueSquare)
-//         removeHighLightColor(greenSquare)
-//         if (inventory.red > 0) {
-//             currentColor = "red";
-//         }
-//     }
-//     if (target === "green") {
-//         highLightColor(greenSquare)
-//         removeHighLightColor(blueSquare)
-//         removeHighLightColor(redSquare)
-//         if (inventory.green > 0) {
-//             currentColor = "green";
-//         }
-//     }
-//     if (target === "blue") {
-//         highLightColor(blueSquare)
-//         removeHighLightColor(redSquare)
-//         removeHighLightColor(greenSquare)
-//         if (inventory.blue > 0) {
-//             currentColor = "blue";
-//         }
-//     }
-// })
 function selectColor(target, colorSquare, inventoryColor) {
-    removeHighLightColor(redSquare);
-    removeHighLightColor(greenSquare);
-    removeHighLightColor(blueSquare);
-    highLightColor(colorSquare);
+  removeHighLightColor(redSquare);
+  removeHighLightColor(greenSquare);
+  removeHighLightColor(blueSquare);
+  removeHighLightColor(eraser);
+  removeHighLightColor(whiteboard);
+  highLightColor(colorSquare);
 
-    if (inventoryColor > 0) {
-        currentColor = target;
-    }
+  if (inventoryColor > 0) {
+    currentColor = target;
+  }
 }
 inv.addEventListener("click", (e) => {
-    const target = e.target.id;
+  const target = e.target.id;
 
-    if (target === "red") {
-        selectColor("red", redSquare, inventory.red);
-    } else if (target === "green") {
-        selectColor("green", greenSquare, inventory.green);
-    } else if (target === "blue") {
-        selectColor("blue", blueSquare, inventory.blue);
-    }
+  if (target === "red") {
+    selectColor("red", redSquare, inventory.red);
+  } else if (target === "green") {
+    selectColor("green", greenSquare, inventory.green);
+  } else if (target === "blue") {
+    selectColor("blue", blueSquare, inventory.blue);
+  }
 });
-
-// redSquare.addEventListener("click", (e) => {
-//     const clicked = e.target.id
-//     if (clicked === 'red') {
-//         removeHighLightColor(blueSquare)
-//         removeHighLightColor(greenSquare)
-//     }
-//     highLightColor(redSquare)
-
-//     if (inventory.red > 0) {
-//         currentColor = "red";
-//     }
-// });
-
-// blueSquare.addEventListener("click", (e) => {
-//     const clicked = e.target.id
-//     if (clicked === 'blue') {
-//         removeHighLightColor(redSquare)
-//         removeHighLightColor(greenSquare)
-//     }
-//     highLightColor(blueSquare)
-//     if (inventory.blue > 0) {
-//         currentColor = "blue";
-//     }
-// });
-
-// greenSquare.addEventListener("click", (e) => {
-//     const clicked = e.target.id
-
-//     if (clicked === 'green') {
-//         removeHighLightColor(redSquare)
-//         removeHighLightColor(blueSquare)
-//     }
-//     highLightColor(greenSquare)
-
-//     if (inventory.green > 0) {
-//         currentColor = "green";
-//     }
-
-// });
 
 grid.addEventListener("click", (event) => {
-    const gridItem = event.target;
-    if (currentColor === "eraser") {
-        const itemColor = gridItem.style.backgroundColor;
-        if (itemColor === "blue" || itemColor === "green") {
-            gridItem.style.backgroundColor = "white";
-            if (itemColor === "blue") {
-                inventory.blue++;
-            } else if (itemColor === "green") {
-                inventory.green++;
-            }
-            updateCounters();
-        }
-    } else if (currentColor === "whiteboard") {
-        const itemColor = gridItem.style.backgroundColor;
-        if (itemColor === "red") {
-            gridItem.style.backgroundColor = "white";
-            inventory.red++;
-            updateCounters();
-        }
-    } else if (currentColor) {
-        const itemColor = gridItem.style.backgroundColor;
-        if (itemColor === "white") {
-            if (inventory[currentColor] > 0) {
-                gridItem.style.backgroundColor = currentColor;
-                inventory[currentColor]--;
-                updateCounters();
-            }
-        }
+  const gridItem = event.target;
+  if (currentColor === "eraser") {
+    const itemColor = gridItem.style.backgroundColor;
+    if (itemColor === "blue" || itemColor === "green") {
+      gridItem.style.backgroundColor = "white";
+      if (itemColor === "blue") {
+        inventory.blue++;
+      } else if (itemColor === "green") {
+        inventory.green++;
+      }
+      updateCounters();
     }
-});
-
-
-redSquare.addEventListener("click", () => {
-    if (inventory.red > 0) {
-        currentColor = "red";
+  } else if (currentColor === "whiteboard") {
+    const itemColor = gridItem.style.backgroundColor;
+    if (itemColor === "red") {
+      gridItem.style.backgroundColor = "white";
+      inventory.red++;
+      updateCounters();
     }
-});
-
-blueSquare.addEventListener("click", () => {
-    if (inventory.blue > 0) {
-        currentColor = "blue";
+  } else if (currentColor) {
+    const itemColor = gridItem.style.backgroundColor;
+    if (itemColor === "white") {
+      if (inventory[currentColor] > 0) {
+        gridItem.style.backgroundColor = currentColor;
+        inventory[currentColor]--;
+        updateCounters();
+      }
     }
-});
-
-greenSquare.addEventListener("click", () => {
-    if (inventory.green > 0) {
-        currentColor = "green";
-    }
+  }
 });
 
 updateCounters();
-
-
